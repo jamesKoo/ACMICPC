b0c9526d934874a921dfa6aa5a153e1cdaab865b
jamesKoo <kjamyoung@gmail.com>
2016-08-25 19:19:39 +0900

    2577 - 숫자의 개수(https://www.acmicpc.net/problem/2577)

---
+++ 2577_숫자의 개수.cpp
@@ -0,0 +1,31 @@
+﻿/*
+2577 - 숫자의 개수(https://www.acmicpc.net/problem/2577)
+
+최대 입력값  1000 x 1000 x 1000 = 1,000,000,000
+    numeric_limits<int>::max() = 2,147,483,647
+*/
+
+#include<cstdio>
+
+int main()
+{
+	// Input
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	// Counting
+	int abc = a * b * c;
+	int cnts[10] = { 0 };
+
+	while (abc != 0)
+	{
+		cnts[abc % 10]++;
+		abc /= 10;
+	}
+
+	// Output
+	for (auto cnt : cnts)
+		printf("%d\n", cnt);
+	
+	return 0;
+}
