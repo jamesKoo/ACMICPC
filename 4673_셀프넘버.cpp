f4841b9620c6344cc1278a8dab415e980c4655b2
jamesKoo <kjamyoung@gmail.com>
2016-08-28 16:06:47 +0900

    4673 - 셀프 넘버(https://www.acmicpc.net/problem/4673)

---
+++ 4673_셀프넘버.cpp
@@ -0,0 +1,43 @@
+﻿/*
+4673 - 셀프 넘버(https://www.acmicpc.net/problem/4673)
+*/
+
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int d(int n)
+{
+	int rst = n;
+	while(n)
+	{
+		rst += n % 10;
+		n /= 10;
+	} 
+	return rst;
+}
+
+int main()
+{
+	// Init
+	int N = 10000;
+	vector<bool> bSelfNums(N + 1, true);
+
+	// Find self number
+	for (int n = 1; n <= N; n++)
+	{
+		int dn = d(n);
+		if (dn <= N)
+			bSelfNums[dn] = false;
+	}
+
+	// Output
+	for (int n = 1; n <= N; n++)
+	{
+		if (bSelfNums[n])
+			printf("%d\n", n);
+	}
+	
+	return 0;
+}
