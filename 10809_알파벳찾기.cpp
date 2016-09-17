26a319547f45515399cf42b56f3f13b34cef028a
jamesKoo <kjamyoung@gmail.com>
2016-09-17 18:10:42 +0900

    10809 - 알파벳 찾기(https://www.acmicpc.net/problem/10809)

---
+++ 10809_알파벳찾기.cpp
@@ -0,0 +1,36 @@
+﻿/*
+10809 - 알파벳 찾기(https://www.acmicpc.net/problem/10809)
+
+정의 : 첫 시작하는 알파벳 위치 찾기
+  
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> cnts(26, -1);
+
+	// Input
+	string str;
+	cin >> str;
+	
+	// Count
+	int len = str.length();
+	for(int idx = 0; idx < str.length(); idx++)
+	{
+		int pos = str[idx] - 'a';
+		if (cnts[pos] == -1)
+			cnts[pos] = idx;
+	}
+
+	// Print
+	for (auto cnt : cnts)
+		cout << cnt << " ";
+
+	return 0;
+}
