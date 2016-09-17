80156cf72fd51fc654ddcd8395c7a0ab1415ff38
jamesKoo <kjamyoung@gmail.com>
2016-09-17 16:30:41 +0900

    11654 - 아스키 코드(https://www.acmicpc.net/problem/11654)

---
+++ 11654_아스키코드.cpp
@@ -0,0 +1,20 @@
+﻿/*
+11654 - 아스키 코드(https://www.acmicpc.net/problem/11654)
+
+정의 : 단순한 아스키 코드 출력 문제
+  
+*/
+
+#include <cstdio>
+
+int main()
+{
+	// input
+	char ch;
+	scanf("%c", &ch);
+
+	// print
+	printf("%d", ch);
+	
+	return 0;
+}
