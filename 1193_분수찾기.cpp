72fbea84d7e96a41485509c374c61a9f5a6f3ec7
jamesKoo <kjamyoung@gmail.com>
2016-09-20 21:11:29 +0900

    1193 - 분수찾기(https://www.acmicpc.net/problem/1193)

---
+++ 1193_분수찾기.cpp
@@ -0,0 +1,38 @@
+﻿/*
+1193 - 분수찾기(https://www.acmicpc.net/problem/1193)
+
+정의 : 분수가 증가하는 규칙을 찾는 문제
+풀이 
+  - [1/1], [1/2, 2/1], [3/1, 2/2, 1/3], [1/4, 2/3, 3/2, 4/1]           
+  - 1, 2, 3, 4, ...의 묶음으로 증가
+  - 각 묶음에서 분자와 분모는 1씩 증가하거나 감소.
+  - 입력 X는 몇 번째 묶음에서 몇 번째 인지를 계산.
+*/
+
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// Input
+	int x;
+	scanf("%d", &x);
+
+	// Find a step(group)
+	int step = 0;
+	while (x -= step++, x > step);
+	
+	// Find a fraction
+	int numerator = x;
+	int denominator = step - x + 1;
+
+	if (step % 2 != 0)
+		swap(numerator, denominator);
+
+	// Output
+	printf("%d/%d", numerator, denominator);
+
+	return 0;
+}
