e6250d282a1cea0417facde0f3d9bc632bd8d168
jamesKoo <kjamyoung@gmail.com>
2016-11-06 15:08:37 +0900

    3046 - R2(https://www.acmicpc.net/problem/3046)

---
+++ 3046_R2.cpp
@@ -0,0 +1,24 @@
+﻿/*
+3046 - R2(https://www.acmicpc.net/problem/3046)
+
+정의
+   - 단순 계산 문제
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	int r, s;
+	cin >> r >> s;
+
+	cout << 2 * s - r;
+	
+	return 0;
+}
