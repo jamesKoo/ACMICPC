debb9646b8647b9ec5fcffd3e9e4b14667a31310
jamesKoo <kjamyoung@gmail.com>
2016-11-06 15:08:26 +0900

    2475 - 검증수(https://www.acmicpc.net/problem/2475)

---
+++ 2475_검증수.cpp
@@ -0,0 +1,34 @@
+﻿/*
+2475 - 검증수(https://www.acmicpc.net/problem/2475)
+
+정의
+   - 단순 계산 문제
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	const int CNT = 5;
+	const int DIV = 10;
+	int n;
+	int vn = 0;
+
+	for (int idx = 0; idx < CNT; idx++)
+	{
+		cin >> n;
+		vn += n*n;
+	}
+
+	vn %= DIV;
+	cout << vn;
+
+
+	return 0;
+}
