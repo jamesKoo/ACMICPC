902b8d03b78dd3420dd8228c7fd5224936d532e9
jamesKoo <kjamyoung@gmail.com>
2016-11-07 22:54:00 +0900

    1085 - 직사각형에서 탈출(https://www.acmicpc.net/problem/1085)

---
+++ 1085_직사각형에서탈출.cpp
@@ -0,0 +1,24 @@
+﻿/*
+1085 - 직사각형에서 탈출(https://www.acmicpc.net/problem/1085)
+
+정의
+   - 단순 계산 문제
+
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	cout << min( min(x, y), min(w - x, h - y) );
+	
+	return 0;
+}
