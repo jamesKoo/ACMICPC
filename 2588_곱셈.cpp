b19ddedded3088c9f7dbbaa1803b8fe4b6c6bf1f
jamesKoo <kjamyoung@gmail.com>
2016-11-22 23:09:37 +0900

    https://www.acmicpc.net/problem/2588

---
+++ 2588_곱셈.cpp
@@ -0,0 +1,29 @@
+﻿/*
+2588 - 곱셈(https://www.acmicpc.net/problem/2588)
+
+정의 - 간단한 계산 문제
+   
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	
+	
+	int num1, num2;
+	cin >> num1 >> num2;
+
+	
+	for (int pos = 0; pos < 3; pos++)
+		cout << num1 * ((num2 / (int)pow(10.0, pos)) % 10) << '\n';
+	
+	cout << num1 * num2;
+		
+	return 0;
+}
