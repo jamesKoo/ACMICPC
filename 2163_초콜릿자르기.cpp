e1ceb3901dbf02ab6568b9f0d4da98058d28a786
jamesKoo <kjamyoung@gmail.com>
2016-11-27 17:40:52 +0900

    2163 - 초콜릿 자르기(https://www.acmicpc.net/problem/2163)

---
+++ 2163_초콜릿자르기.cpp
@@ -0,0 +1,22 @@
+﻿/*
+2163 - 초콜릿 자르기(https://www.acmicpc.net/problem/2163)
+
+유형 - 단순 계산 문제
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+	cout << n * m - 1;
+	
+	return 0;
+}
