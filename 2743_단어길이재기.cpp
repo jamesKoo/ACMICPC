3d6f16af3aaf736d86e7925b43acd7570a217f64
jamesKoo <kjamyoung@gmail.com>
2016-11-27 16:10:25 +0900

    2743 - 단어 길이 재기(https://www.acmicpc.net/problem/2743)

---
+++ 2743_단어길이재기.cpp
@@ -0,0 +1,22 @@
+﻿/*
+2743 - 단어 길이 재기(https://www.acmicpc.net/problem/2743)
+
+유형 - 단순한 문자열 처리
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+
+	string str;
+	cin >> str;
+	cout << str.length();
+	
+	return 0;
+}
